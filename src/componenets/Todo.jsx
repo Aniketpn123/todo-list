385b57e7d5499eea91344fec3d1e2ceafe220731
ANIKET <aniketpn123@gmail.com>
2024-10-10 21:53:23 +0530

    eyery

--- src/componenets/Todo.jsx
+++ src/componenets/Todo.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
 export default function Todo() {
-  return <div>
-    <form action="">
-      <input type="text" />
-      <button type="submit">Add</button>
-    </form>
-  </div>;
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  function Predefault(e) {
+    e.preventDefault();
+  }
+  return (
+    <div>
+      <form action="" onSubmit={Predefault}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
